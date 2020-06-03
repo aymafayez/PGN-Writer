2f868a6225d8a06c622ffa276e4423abe3f8c686
aymanFayez <aymanfayez277@gmail.com>
2020-06-03 20:15:40 +0200

    Initial Commit

---
+++ PGN-Writer/PGN_Writer.h
@@ -0,0 +1,19 @@
+//
+//  PGN_Writer.h
+//  PGN-Writer
+//
+//  Created by A.F on 6/3/20.
+//  Copyright © 2020 A.F. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PGN_Writer.
+FOUNDATION_EXPORT double PGN_WriterVersionNumber;
+
+//! Project version string for PGN_Writer.
+FOUNDATION_EXPORT const unsigned char PGN_WriterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PGN_Writer/PublicHeader.h>
+
+
